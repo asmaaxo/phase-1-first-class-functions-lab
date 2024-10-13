97943f1d6ef10595070c4403dd239329e39a10d2
Asmaa Abdi <asmaaabdi03@gmail.com>
2024-10-13 17:03:44 +0300

    phase-1-first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+  return (fare) => fare * multiplier;
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, selectedFunction) => {
+  return selectedFunction(drivers);
+};
+
+module.exports = {
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers,
+  selectingDrivers,
+  createFareMultiplier,
+  fareDoubler,
+  fareTripler,
+  selectDifferentDrivers
+};
